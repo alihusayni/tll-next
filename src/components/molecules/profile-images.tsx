5fc9fed3e14730e4dc9a81637e59bc21ca7da929
adkoy <razimanesh.yalda@gmail.com>
2025-10-18 17:29:51 +0330

    about section done

--- src/components/molecules/profile-images.tsx
+++ src/components/molecules/profile-images.tsx
@@ -2,21 +2,24 @@ import Image from 'next/image';
 
 export default function ProfileImages() {
   return (
-    <div className="flex flex-col items-center gap-4 lg:relative lg:h-[572px] lg:w-[700px]">
-      <Image
-        src="/assets/about/about_img_consult.png"
-        alt="Consultation image"
-        width={332}
-        height={412}
-        className="w-[332px] h-[412px] lg:absolute lg:top-[27px] lg:right-0 lg:w-[332px] lg:h-[412px] rounded-[16px] object-cover grayscale"
-      />
-      <Image
-        src="/assets/about/about_img_Tuan.png"
-        alt="Tuan Le profile image"
-        width={332}
-        height={412}
-        className="w-[332px] h-[412px] lg:absolute lg:top-[131px] lg:left-0 lg:w-[332px] lg:h-[412px] rounded-[16px] object-cover grayscale lg:transform-none transform translate-x-1/2"
-      />
-    </div>
+      <div className="relative h-[35.75rem] w-[43.75rem]">
+
+          <Image
+              src="/assets/about/about_img_consult.png"
+              alt="Consultation image"
+              width={332}
+              height={412}
+              className="absolute top-[1.6875rem] right-0 w-[20.75rem] h-[25.75rem] rounded-[16px] object-cover grayscale border mix-blend-luminosity"
+          />
+
+          <Image
+              src="/assets/about/about_img_Tuan.png"
+              alt="Tuan Le profile image"
+              width={332}
+              height={412}
+              className="absolute top-[6.75rem] -left-40 w-[20.75rem] h-[25.75rem] rounded-[16px] object-cover grayscale border mix-blend-luminosity transform translate-x-1/2"
+          />
+      </div>
+
   );
 }
--- src/components/organisms/about-section.tsx
+++ src/components/organisms/about-section.tsx
@@ -3,25 +3,27 @@ import ProfileStats from '../molecules/profile-stats';
 import ProfileImages from '../molecules/profile-images';
 
 export default function AboutSection() {
-  return (
-    <section id="about" className="bg-[#E8EDF2] py-32 px-16 m-0 scroll-mt-24">
-      <div className="max-w-[90rem] mx-auto flex flex-col items-center gap-32">
-        <div className="flex flex-col lg:flex-row justify-between items-start gap-8 w-full">
-          <div className="flex flex-col gap-32 lg:pr-8">
-            <h2 className="font-inter-tight font-semibold text-[52px] leading-[1.1538461538461537] tracking-[-0.02em] text-[#071C32]">
-              Immigration Services by Top Immigration Attorney.
-            </h2>
-            <ProfileStats />
-            <p className="font-inter font-normal text-lg leading-[1.2222222222222223] text-[#071C32]">
-              A Bay Area native, Tuan Le holds Finance and MIS degrees from San Jose State University.
-              <br />
-              He has worked as a law clerk, legal assistant, and associate attorney.
-            </p>
-            <CtaButton text="Get in touch" />
-          </div>
-          <ProfileImages />
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section id="about" className="bg-[#E8EDF2] py-32 px-16 m-0 scroll-mt-24 overflow-x-clip">
+            <div className="max-w-[90rem] mx-auto flex flex-col items-center gap-32">
+                <div className="flex flex-col lg:flex-row justify-between items-start gap-8 w-full">
+                    <div className="flex flex-col gap-25">
+                        <h2 className="font-inter-tight font-semibold text-[52px] lg:max-w-[40rem] leading-[60px] tracking-[-0.02em] text-[#071C32]">
+                            Immigration Services by Top Immigration Attorney.
+                        </h2>
+                        <div className="flex flex-col gap-8 lg:pr-8 max-w-[27.5rem]">
+                            <ProfileStats/>
+                            <p className="font-inter font-normal text-lg leading-[22px] text-[#071C32] ">
+                                A Bay Area native, Tuan Le holds Finance and MIS degrees from San Jose State University.
+                                <br/>
+                                He has worked as a law clerk, legal assistant, and associate attorney.
+                            </p>
+                            <CtaButton text="Get in touch"/>
+                        </div>
+                    </div>
+                    <ProfileImages/>
+                </div>
+            </div>
+        </section>
+    );
 }
