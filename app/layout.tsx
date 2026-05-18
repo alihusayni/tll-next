596d5ccbf35e129fb3170a536751458e9453a849
alihusayni <ali.h@toporganicleads.com>
2026-05-18 16:02:44 +0300

    replace GA4 with GTM-KP4TXX5K

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,26 +79,29 @@ export default function RootLayout({
                 />
                 <AcsbScript />
 
-                {/* GA4 — lazyOnload keeps gtag off the critical render path */}
+                {/* Google Tag Manager */}
                 <Script
-                    id="_next-ga-init"
-                    strategy="lazyOnload"
+                    id="gtm-script"
+                    strategy="afterInteractive"
                     dangerouslySetInnerHTML={{
-                        __html: `
-                            window['dataLayer'] = window['dataLayer'] || [];
-                            function gtag(){window['dataLayer'].push(arguments);}
-                            gtag('js', new Date());
-                            gtag('config', 'G-9CL0P20FC0');
-                        `,
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KP4TXX5K');`,
                     }}
-                />
-                <Script
-                    id="_next-ga"
-                    strategy="lazyOnload"
-                    src="https://www.googletagmanager.com/gtag/js?id=G-9CL0P20FC0"
                 />
             </head>
             <body className={`${interTight.variable} ${inter.variable} antialiased`}>
+                {/* Google Tag Manager (noscript) */}
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-KP4TXX5K"
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    />
+                </noscript>
                 {children}
             </body>
         </html>
