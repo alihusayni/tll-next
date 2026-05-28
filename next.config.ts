a19d906b9e3730dd7832d34eadc722436739b100
alihusayni <ali.h@toporganicleads.com>
2026-05-28 10:31:18 +0300

    fix LCP: remove broken preload/dead preconnects, enable AVIF

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,7 @@ const nextConfig: NextConfig = {
     return config;
   },
   images: {
+    formats: ['image/avif', 'image/webp'],
     deviceSizes: [480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
 
     remotePatterns: [
